b3feccfa516527cd0deb4f45b80a91e2d0b87245
Reed Taylor <reed.taylor@me.com>
2018-07-24 12:42:47 -0700

    small frame changes

--- src/showframe.py
+++ src/showframe.py
@@ -9,7 +9,7 @@ def start():
     if (curdir == ""):
         curdir = os.getcwd()
 
-def get_8bit_frame(frame):
+def get_8bit_frame(frame): #represnt thermal image in rgb space
     output = frame >> 2
     output[output >= 4096] = 4096 - 1
     resized = np.zeros((240,156))
@@ -24,14 +24,23 @@ while (1):
     if (curframe == "n"):
         start()
     else:
-        rgb_frame = np.load(curdir+"/rgb_full_vid/rgb_frame_"+curframe+".npy")
-        depth = np.load(curdir+"/depth_full_vid/depth_frame_"+curframe+".npy")
-        ir = np.load(curdir+"/ir_full_vid/ir_frame_"+curframe+".npy")
+        try: #load and display frame
+            rgb_frame = np.load(curdir+"/rgb_full_vid/rgb_frame_"+curframe+".npy")
+            depth = np.load(curdir+"/depth_full_vid/depth_frame_"+curframe+".npy")
+            ir = np.load(curdir+"/ir_full_vid/ir_frame_"+curframe+".npy")
 
-        ir_frame = get_8bit_frame(ir)
-        depth_frame = np.uint8(depth.astype(float) * 255 / 2**12 - 1)
-        depth_frame = 255 - cv2.cvtColor(depth_frame, cv2.COLOR_GRAY2RGB)
+            ir_frame = get_8bit_frame(ir)
+            depth_frame = np.uint8(depth.astype(float) * 255 / 2**12 - 1)
+            depth_frame = 255 - cv2.cvtColor(depth_frame, cv2.COLOR_GRAY2RGB)
 
-        disp = np.hstack((rgb_frame, depth_frame, ir_frame))
-        cv2.imshow('frame', disp)
-        cv2.waitKey(500)
+            disp = np.hstack((rgb_frame, depth_frame, ir_frame))
+            cv2.imshow('frame', disp)
+            cv2.waitKey(500)
+
+        except: #print out valid frames
+            files = os.listdir(curdir+"/rgb_full_vid/")
+            for i in range(len(files)):
+                files[i] = files[i].split('_')[2].strip(".npy")
+            files.sort(key=int)
+            print("Invalid frame number. Valid frames are:", files)
+
